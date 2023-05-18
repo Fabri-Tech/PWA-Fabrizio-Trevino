c43c161e267f89958f44d4762a5fddcfbe1e054d
fabri-tech <fabriziotrevino@gmail.com>
2023-05-17 20:53:18 -0600

    fixed installers

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,29 @@
-const butInstall = document.getElementById('buttonInstall');
+const buttonInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+const handleBeforeInstallPrompt = (event) => {
+  window.deferredPrompt = event;
+  buttonInstall.classList.remove('hidden');
+};
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+const handleClickInstall = async () => {
+  const promptEvent = window.deferredPrompt;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+  buttonInstall.classList.add('hidden');
+};
+
+const handleAppInstalled = (event) => {
+  window.deferredPrompt = null;
+};
+
+// Add event handlers
+window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+buttonInstall.addEventListener('click', handleClickInstall);
+window.addEventListener('appinstalled', handleAppInstalled);
